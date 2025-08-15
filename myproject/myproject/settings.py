6bdbe2a5774a079d01f4d37795803f3946256bcc
Sani Yadav <kltechitsolution@gmail.com>
2025-08-15 19:07:46 +0530

    fix(static): use absolute STATIC_URL '/static/' so CSS/JS load correctly on nested routes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+# Use absolute path to ensure correct URLs on all routes (e.g., /about.html)
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "webapp" / "static",
 ]
